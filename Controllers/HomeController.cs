95a765ab6d5f0bd330821e0faf11dc64a917f620
Austin Bruso <austin.bruso@hotmail.com>
2022-06-13 16:08:36 -0500

    Added Dashboard View Model

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,16 +6,21 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using TheBugTrackerProject.Extensions;
+using TheBugTrackerProject.Models.ViewModel;
+using TheBugTrackerProject.Services.Interfaces;
 
 namespace BugTrackerProject.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IBTCompanyInfoService _companyInfoService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IBTCompanyInfoService companyInfoService)
         {
             _logger = logger;
+            _companyInfoService = companyInfoService;
         }
 
         public IActionResult Index()
@@ -23,6 +28,22 @@ namespace BugTrackerProject.Controllers
             return View();
         }
 
+
+        public async Task<IActionResult> Dashboard()
+        {
+            DashboardViewModel model = new();
+            int companyId = User.Identity.GetCompanyId().Value;
+          
+
+            model.Company = await _companyInfoService.GetCompanyInfoById(companyId);
+            model.Projects = (await _companyInfoService.GetAllProjectsAsync(companyId)).Where(p => p.Archived == false).ToList();
+            model.Tickets = model.Projects.SelectMany(p => p.Tickets).Where(t => t.Archived == false).ToList();
+            model.Members = model.Company.Members.ToList();
+
+            return View(model);
+
+
+        }
         public IActionResult Privacy()
         {
             return View();
---
+++ Models/ViewModel/DashboardViewModel.cs
@@ -0,0 +1,18 @@
+﻿using BugTrackerProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBugTrackerProject.Models.ViewModel
+{
+    public class DashboardViewModel
+    {
+
+        public Company Company { get; set; }
+        public List<Project> Projects { get; set; }
+        public List<Ticket> Tickets { get; set; }
+        public List<BTUser> Members { get; set; }
+
+    }
+}
